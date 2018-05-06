09004fac632dca8a55b94fe3b3b587e2a9a6cfee
Puneet Behl <behl4485@gmail.com>
2018-05-06 22:51:49 +0530

    Multi-field creating untouched

---
+++ src/integration-test/groovy/testapp/search/MultiFieldUntouchedSpec.groovy
@@ -0,0 +1,33 @@
+package testapp.search
+
+import grails.plugins.elasticsearch.ElasticSearchResult
+import spock.lang.Specification
+import testapp.ElasticSearchSpec
+import testapp.component.Spaceship
+import testapp.multifield.Person
+
+class MultiFieldUntouchedSpec extends Specification implements ElasticSearchSpec {
+
+    void 'Multi_filed creates untouched field'() {
+        given:
+        Person mal = save new Person(firstName: 'J. T.', lastName: 'Esteban')
+        Spaceship spaceship = save new Spaceship(name: 'USS Grissom', captain: mal)
+
+        index(spaceship)
+        refreshIndices()
+
+        when:
+        ElasticSearchResult results = search(Spaceship) {
+            bool { must { term("name.untouched": 'USS Grissom') } }
+        }
+
+        then:
+        results.total == 1
+
+        Spaceship ship = results.searchResults.first() as Spaceship
+        ship.name == 'USS Grissom'
+        ship.captain.firstName == 'J. T.'
+        ship.captain.lastName == 'Esteban'
+    }
+
+}
